621837c87458b6bbea556f59a3777640ed1fc98a
jovitawaisakhi <waisakhijovita@gmail.com>
2024-08-30 16:06:14 +0700

    add: search product

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,14 +4,19 @@ import IconCart from '@assets/icons/IconCart';
 import IconHeart from '@assets/icons/IconHeart';
 import IconPerson from '@assets/icons/IconPerson';
 import { Link, useNavigate } from 'react-router-dom';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import UserPopUp from './UserPopup';
 import SearchModal from './SearchModal';
+import { getAllProductsQuery, getProductImageQuery } from '@/services/productService';
+import TypeUtils from '@utils/TypeUtils';
 
 const Navbar: React.FC = () => {
     const navigate = useNavigate();
     const [isUserPopupOpen, setIsUserPopupOpen] = useState(false);
     const [isSearchModalOpen, setIsSearchModalOpen] = useState(false);
+    const [productImageUrls, setProductImageUrls] = useState<Map<number, string>>(new Map());
+    const { getProductImage } = getProductImageQuery();
+    const { products, getAllProduct } = getAllProductsQuery();
 
     const togglePopup = (event: React.MouseEvent) => {
         event.stopPropagation();
@@ -21,6 +26,25 @@ const Navbar: React.FC = () => {
     const toggleSearchModal = () => {
         setIsSearchModalOpen(prev => !prev);
     };
+
+    async function fetchProducts() {
+        const products = await getAllProduct();
+        products?.forEach(async p => {
+            const image = await getProductImage([p.id]);
+            if (!image || image.length === 0) {
+                return;
+            };
+            setProductImageUrls(prev => {
+                const newMap = new Map(prev);
+                newMap.set(Number(p.id), TypeUtils.byteArrayToImageURL(image[0]));
+                return newMap;
+            })
+        })
+    }
+
+    useEffect(() => {
+        fetchProducts();
+    }, [])
 
     return (
         <nav className="sticky top-0 flex w-screen items-center justify-between p-10 mb-5 bg-white h-[125px] shadow-md z-40">
@@ -39,7 +63,7 @@ const Navbar: React.FC = () => {
                 </div>
             </div>
 
-            <SearchModal isOpen={isSearchModalOpen} onClose={toggleSearchModal}/>
+            <SearchModal isOpen={isSearchModalOpen} onClose={toggleSearchModal} products={products} productImageUrls={productImageUrls}/>
         </nav>
     );
 };
--- src/components/SearchModal.tsx
+++ src/components/SearchModal.tsx
@@ -1,69 +1,67 @@
-import { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
-import image from "../assets/product/login.jpg";
 import Product from "@models/product";
-import { getAllProductsQuery, getProductImageQuery } from "@/services/productService";
-import TypeUtils from "@utils/TypeUtils";
+import IconSearch from "@assets/icons/IconSearch";
+import { useEffect, useState } from "react";
 
 interface Props {
     isOpen: boolean;
     onClose: () => void;
+    products: Product[];
+    productImageUrls: Map<number, string>;
 }
 
-const SearchModal:React.FC<Props> = ({ isOpen, onClose }) => {
-    const [searchQuery, setSearchQuery] = useState('');
-    const [productImageUrls, setProductImageUrls] = useState<Map<number, string>>(new Map());
-    const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
-    const { getProductImage } = getProductImageQuery();
-    const { products, getAllProduct, getAllProductsLoading } = getAllProductsQuery();
+const SearchModal:React.FC<Props> = ({ isOpen, onClose, products, productImageUrls }) => {
+    const [searchQuery, setSearchQuery] = useState<string>('');
+    const [searchResult, setSearchResult] = useState<Product[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
 
     if (!isOpen) return null;
 
-    const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        console.log(searchQuery);
-        onClose();
+    const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const query = e.target.value;
+        setIsLoading(true);
+
+        const filteredProducts = products.filter(product =>
+            product.name.toLowerCase().includes(query.toLowerCase()) ||
+            product.clothingType.toLowerCase().includes(query.toLowerCase())
+        );
+        setSearchResult(filteredProducts);
+        setIsLoading(false);
     };
 
-    async function fetchProducts() {
-        const products = await getAllProduct();
-        products?.forEach(async p => {
-            const image = await getProductImage([p.id]);
-            if (!image || image.length === 0) {
-                return;
-            };
-            setProductImageUrls(prev => {
-                const newMap = new Map(prev);
-                newMap.set(Number(p.id), TypeUtils.byteArrayToImageURL(image[0]));
-                return newMap;
-            })
-        })
-    }
-
-    useEffect(()=>{
-        fetchProducts();
-    }, [])
-
     return(
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-            <div className="bg-white p-6 rounded-md shadow-lg w-1/3 relative">
+        <div className="fixed px-32 py-20 inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+            <div className="bg-white p-6 rounded-md shadow-lg w-full relative">
                 <button onClick={onClose} className="absolute top-3 right-3 text-xl">x</button>
-                <h2 className="text-xl mb-4">Search</h2>
-                <form onSubmit={handleSearch}>
+                <div className="flex justify-center items-center mt-3 mx-[2.5%] px-[2%] mb-10 border border-gray-300 rounded-md">
+                    <div className="w-5 h-5">
+                        <IconSearch />
+                    </div>
+                    
                     <input
                         type="text"
                         value={searchQuery}
-                        onChange={(e) => setSearchQuery(e.target.value)}
+                        onChange={(e) => {
+                            setSearchQuery(e.target.value)
+                            handleSearch(e);
+                        }}
                         placeholder="Search for products..."
-                        className="w-full p-3 border border-gray-300 rounded-md"
+                        className="w-full p-3 focus:outline-none"
                     />
-                </form>
 
-                <div className="grid grid-cols-5 gap-x-[3.5%] gap-y-8 px-[2.5%] w-full">
-                    {products.map((p, idx) => (
-                        <ProductCard imageUrl={image} product={p} key={idx}/>
-                    ))}
                 </div>
+
+                {isLoading?
+                    <div className="flex justify-center items-center animate-pulse">
+                        Loading...
+                    </div>
+                :
+                    <div className="grid grid-cols-5 gap-x-[3.5%] gap-y-8 px-[2.5%] w-full">
+                        {searchResult?.map((p, idx) => (
+                            <ProductCard imageUrl={productImageUrls.get(p.id)} product={p} key={idx}/>
+                        ))}
+                    </div>
+                }
             </div>
         </div>
     )
